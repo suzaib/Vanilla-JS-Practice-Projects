0f5864da4e53ab6eb1d29bd768439af28f18a3b7
Suzaib <160600583+suzaib@users.noreply.github.com>
2024-08-06 13:22:16 +0530

    index.js

---
+++ Digital Clock/index.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Digital Clock</title>
+    <link rel="stylesheet" href="style.css">
+    <script defer src="app.js"></script>
+</head>
+<body>
+    <div class="hero">
+        <div class="container">
+            <div class="clock">
+                <div id="time"></div>
+                <div id="attribute">
+                    <p id="hr">Hr</p>
+                    <p id="min">Min</p>
+                    <p id="sec">Sec</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
