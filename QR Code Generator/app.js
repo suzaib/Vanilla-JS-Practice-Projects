71270052d9347c24c75c4e6dd8c18a3c564cc8f3
Suzaib <160600583+suzaib@users.noreply.github.com>
2024-10-15 00:20:14 +0530

    Update app.js

--- QR Code Generator/app.js
+++ QR Code Generator/app.js
@@ -1,21 +1,31 @@
+//Accessing our HTML Elements
 let imgBox=document.querySelector("#imgBox");
 let qrImage=document.querySelector("#qrImage");
 let qrText=document.querySelector("#qrText");
-let button=document.querySelector("button");;
+let button=document.querySelector("button");
 
+//Function to generate QR
 const generateQR=()=>{
+
+    //Works well if search box isn't empty
     if(qrText.value.length>0){
+        //This is a free api that generates a QR for us on the basis of text entered and then we store the QR as image
         qrImage.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrText.value;
         imgBox.classList.add("show-img");
     }
+
+    //If search box is empty we throw an error while vibrating the search box for a eye catching effect
     else{
         qrText.classList.add("error");
+
+        //We set the time for the vibration of the search box
         setTimeout(()=>{
             qrText.classList.remove("error");
         },1000);
     }
 }
 
+//Finally calling the generate QR function when the button is clicked
 button.addEventListener("click",()=>{
     generateQR();
 })
