bb1bc8f171d61475381e9418429793d2261e7318
Suzaib <160600583+suzaib@users.noreply.github.com>
2024-10-15 12:36:42 +0530

    Update app.js

--- PopUp Menu/app.js
+++ PopUp Menu/app.js
@@ -1,14 +1,20 @@
+//Accessing our HTML elements
 let submitButton=document.querySelector("#submit");
 let okButton=document.querySelector("#ok");
 let popup=document.querySelector(".popup")
 
+//Function to open popup by adding class "open-popup"
 const openPopup=()=>{
     popup.classList.add("open-popup")
 }
 
+//Function to close popup by removing the class "open-popup"
 const closePopup=()=>{
     popup.classList.remove("open-popup")
 }
 
+//Attaching Event Listeners to submit Button to call openPopup function to open popup on click
 submitButton.addEventListener("click",openPopup);
+
+//Attaching Event Listener to close button of popup to call the closePopup function on click
 okButton.addEventListener("click",closePopup);
