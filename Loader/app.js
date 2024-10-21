72e3688676d83e3a770a2930fdc8f5e0459ff68f
Suzaib <160600583+suzaib@users.noreply.github.com>
2024-10-21 19:46:27 +0530

    Update app.js

--- Loader/app.js
+++ Loader/app.js
@@ -1,11 +1,18 @@
+//Accessing our HTML Elements
 let number=document.querySelector("#number");
+
+//Initialising counter variable that will show how much loader has progressed
 let counter=0;
 
+//The setInterval function will be called every 30 milliseconds and will update the counter value by 1 until it becomes 65
 setInterval(()=>{
+
+    //When counter value becomes 65 it stops the setInterval function and the counter halts
     if(counter==65){
         clearInterval();
     }
 
+    //Otherwise the counter keeps increasing
     else{
         counter+=1;
         number.innerText=counter+"%";
